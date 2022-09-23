bc5ba4d2af2d1704cec60dfe8e05e50878269d65
Demyankov home_PC <demyantaurus@gmail.ru>
2022-09-23 20:48:04 +0300

    Delete user: server error

--- src/api/deleteUser.ts
+++ src/api/deleteUser.ts
@@ -2,10 +2,14 @@ import axios, { AxiosResponse } from "axios";
 import { UserType } from "../types/userType";
 import { authAxiosInstance } from "./authAxiosInstance";
 
-const apiPath = `${process.env.REACT_APP_API_AUTH}/auth/users`;
+const apiPath = `${process.env.REACT_APP_API_AUTH}/auth/users/me/`;
 
 export async function deleteUser(
   userId: UserType["id"]
 ): Promise<AxiosResponse> {
-  return await authAxiosInstance.delete(`${apiPath}/?id=${userId}`);
+  return await authAxiosInstance({
+    method: "delete",
+    url: apiPath,
+    data: { current_password: "demyan350" },
+  });
 }
