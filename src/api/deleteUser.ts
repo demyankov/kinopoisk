286e01b8ca601c8a22847dc30635c75904298923
Demyankov PC job <demyantaurus@mail.ru>
2022-09-23 16:55:15 +0300

    delete users, attemot 1, not allowed

--- src/api/deleteUser.ts
+++ src/api/deleteUser.ts
@@ -2,10 +2,10 @@ import axios, { AxiosResponse } from "axios";
 import { UserType } from "../types/userType";
 import { authAxiosInstance } from "./authAxiosInstance";
 
-const apiPath = `${process.env.REACT_APP_API_AUTH}/auth/users/me`;
+const apiPath = `${process.env.REACT_APP_API_AUTH}/auth/users`;
 
 export async function deleteUser(
   userId: UserType["id"]
 ): Promise<AxiosResponse> {
-  return await authAxiosInstance.delete(apiPath);
+  return await authAxiosInstance.delete(`${apiPath}/?id=${userId}`);
 }
